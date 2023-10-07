1d59f3450f4b777df9aba643fef5bf0ae38b30fa
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-10-07 15:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import asyncio
+from aiohttp import web
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Crazybossisback'
+def thecrazybossisback():
+    return 'Crazybossss'
 
+async def run_app():
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 5000)
+    await site.start()
 
 if __name__ == "__main__":
-    app.run()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_app())
+    loop.run_forever()
